6187a67eb25f34ea99f50b19df55a11a61e435b4
shaharec <52333216+shaharec@users.noreply.github.com>
2020-03-28 13:08:21 -0400

    Delete prossescmd.h

--- prossescmd.h
+++
@@ -1,8 +0,0 @@
-#include "DataStruct.h"
-#include "Syntax.h"
-#include "FirstPassUtils.h"
-#include "SecondPassUtils.h"
-boolean prossesAsm(const char* fileName);
-boolean firstPass(FILE *fp);
-boolean secondPass(FILE *fp);
-
